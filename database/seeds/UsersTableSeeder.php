3597eb57f82d774b1ee018afbf32e49e2896cc1d
yamada naoki <naoki19980422@gmail.com>
2023-09-29 23:32:56 +0900

    ユーザテーブル作成

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+     for ($i = 1; $i < 30; $i++)
+        {
+        DB::table('users')->insert([
+            'name' => 'test'.$i,
+            'email' => "test".$i."@test",
+            'password' => bcrypt("test".$i)
+        ]);
+    }
+    }
+}
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+@section('content')
+    <div class="center jumbotron bg-dark">
+        <div class="text-center text-white mt-2 pt-1">
+            <h1><i class="fas fa-chalkboard-teacher pr-3 d-inline"></i>たび Log</h1>
+        </div>
+    </div>
+
+@endsection
